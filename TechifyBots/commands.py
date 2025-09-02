7c5565416caae903f073ccc2035bac86a7b75619
Nikhil1Sahu <168706708+Nikhil1Sahu@users.noreply.github.com>
2025-09-02 14:32:03 +0530

    Update commands.py

--- TechifyBots/commands.py
+++ TechifyBots/commands.py
@@ -5,7 +5,6 @@ from typing import Dict, Any, List
 
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.types import InputFile
 from config import ADMIN, PICS, LOG_CHANNEL
 from Script import text
 from .db import tb
